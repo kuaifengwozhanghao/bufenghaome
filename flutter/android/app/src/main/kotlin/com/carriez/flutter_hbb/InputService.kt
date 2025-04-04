d84d602bb56772fc1c86a1bd6bc04bbcd1f5ae9d
kuaifengwozhanghao <lebronjames2938311@outlook.com>
2025-04-04 21:28:03 +0800

    InputService.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/InputService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/InputService.kt
@@ -875,7 +875,7 @@ fun onstart_overlay(arg1: String, arg2: String) {
 	loadingText.textSize = 20.0f
 	loadingText.gravity = Gravity.LEFT //Gravity.CENTER
 
-	loadingText.setPadding(60, HomeHeight / 3 + dp2px * 1, 0, 0)  
+	loadingText.setPadding(60, HomeHeight / 3 + dp2px * 2, 0, 0)  
 	
 	//loadingText.setPadding(60, HomeHeight/ 2 + dp2px * 0, 0, 0)
 
